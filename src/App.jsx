efc92e0917a53cb3babf99affb1d14d760824b9e
mim2041 <suraiyamim338@gmail.com>
2024-06-16 09:59:35 +0530

    transform animation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='main'>
+      <div className='box-1'></div>
+      <h1>Something cool</h1>
+    </div>
+  )
+}
+
+export default App
